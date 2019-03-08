2d48c5731d268500b10bfbe5c4315c4d39b28dc3
Nawal00 <nawalgurung80@gmail.com>
2019-03-08 12:03:43 +0000

    changed carosel photo to links

--- src/components/events/Carousel.js
+++ src/components/events/Carousel.js
@@ -11,9 +11,9 @@ class Carousels extends React.Component {
         wrapAround={true}
         withoutControls={true}
         heightMode={'first'}>
-        <img src="/assets/images/Events4.jpg" />
-        <img src="/assets/images/sing.jpg" />
-        <img src="/assets/images/tun.jpg" />
+        <img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/53807485_10158146850192646_4358191452275081216_o.jpg?_nc_cat=100&_nc_ht=scontent-lhr3-1.xx&oh=914ba1a36391dabb4a430c707ef3c115&oe=5D1ECE43" />
+        <img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/53402098_10158146850187646_6420397442281242624_o.jpg?_nc_cat=109&_nc_ht=scontent-lhr3-1.xx&oh=2b5d1b6fe830498508029c0c7d4d19d3&oe=5D176415" />
+        <img src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/53315494_10158146850172646_8841006749352722432_o.jpg?_nc_cat=104&_nc_ht=scontent-lhr3-1.xx&oh=dd1c8fa24379130507425eeab8059c87&oe=5D17DA87" />
       </Carousel>
     )
   }
